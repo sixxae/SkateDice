35d1a83c16a0d5e7ca1ff9df7a0119008f7a68ec
sixxae <jaspervan_dermeulen@hotmail.com>
2023-04-14 08:37:02 +0200

    What is up my brothers. Here's the initial commit.

--- app/src/main/java/com/example/myapplication/RollerbladingDiceActivity.java
+++ app/src/main/java/com/example/myapplication/RollerbladingDiceActivity.java
@@ -1,25 +1,33 @@
-import com.example.myapplication.R;
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class RollerbladingDiceActivity extends AppCompatActivity {
 
     // Define the options for each die
-    private String[] die1Options = {"One Foot (Left Leading)", "One Foot (Right Leading)", "Two Feet (Left Leading)", "Two Feet (Right Leading)"};
-    private String[] die2Options = {"Leading: Groove positive", "Leading: Groove negative", "Leading: Soul positive", "Leading: Soul negative"};
-    private String[] die3Options = {"Trailing: Groove positive", "Trailing: Groove negative", "Trailing: Soul positive", "Trailing: Soul negative"};
-    private String[] die4Options = {"No-spin", "AO", "Truespin", "Half-cab", "Truespin Half-cab"};
-
+    private final String[] die1Options = {"Makio", "Fishbrain", "Torque", "Backslide", "Fastslide", "Top Mizu", "Top Soul", "Top Acid", "Top Pstar", "Mistrial", "Frontside", "Backside", "FS Royale", "BS Royale", "FS Fahrv", "BS Fahrv", "Unity", "Savannah", "SoulGrind", "Mizu", "Acid", "Pstar", "Mistrial", "X-Grind"};
+    private final String[] die2Options = {"No-spin", "AO", "Truespin", "Half-cab", "Truespin Half-cab"};
+    private final String[] die3Options = {"Direction: ----->", "Direction: <-----"};
     // Define the weights for each option
-    private int[] die1Weights = {1, 1, 1, 1};
-    private int[] die2Weights = {2, 2, 1, 1};
-    private int[] die3Weights = {2, 2, 1, 1};
-    private int[] die4Weights = {3, 2, 1, 1, 1};
+    private final int[] die1Weights = {7, 1, 1, 1, 1, 4, 4, 4, 4, 3, 8, 8, 6, 4, 5, 4, 4, 3, 5, 5, 3, 3, 2, 1};
+    private final int[] die2Weights = {10, 3, 2, 1, 1};
+    private final int[] die3Weights = {1, 1};
 
     // Create a random number generator
-    private Random rand = new Random();
+    private final Random rand = new Random();
 
     // Create a weighted random generator
-    private WeightedRandomGenerator wrand = new WeightedRandomGenerator();
+    private final WeightedRandomGenerator wrand = new WeightedRandomGenerator();
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +37,6 @@ public class RollerbladingDiceActivity extends AppCompatActivity {
         Button die1Button = findViewById(R.id.die1_button);
         Button die2Button = findViewById(R.id.die2_button);
         Button die3Button = findViewById(R.id.die3_button);
-        Button die4Button = findViewById(R.id.die4_button);
         Button resetButton = findViewById(R.id.reset_button);
         TextView resultTextView = findViewById(R.id.result_text_view);
 
@@ -37,60 +44,44 @@ public class RollerbladingDiceActivity extends AppCompatActivity {
         die1Button.setText("Die 1");
         die2Button.setText("Die 2");
         die3Button.setText("Die 3");
-        die4Button.setText("Die 4");
 
         // Set the on click listener for each die button
-        die1Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Roll the die using a weighted random generator
-                rollDie(die1Options, die1Weights);
+        die1Button.setOnClickListener(v -> {
+            // Roll the die using a weighted random generator
+            rollDie(die1Options, die1Weights);
 
-                // Display the result of the roll in the text view
-                resultTextView.setText("Die 1: " + wrand.next());
-            }
+            // Display the result of the roll in the text view
+            resultTextView.setText("Die 1: " + wrand.next());
         });
 
-        die2Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Roll the die using a weighted random generator
-                rollDie(die2Options, die2Weights);
+        die2Button.setOnClickListener(v -> {
+            // Roll the die using a weighted random generator
+            rollDie(die2Options, die2Weights);
 
-                // Display the result of the roll in the text view
-                resultTextView.setText("Die 2: " + wrand.next());
-            }
+            // Display the result of the roll in the text view
+            resultTextView.setText("Die 2: " + wrand.next());
         });
 
-        die3Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Roll the die using a weighted random generator
-                rollDie(die3Options, die3Weights);
+        die3Button.setOnClickListener(v -> {
+            // Roll the die using a weighted random generator
+            rollDie(die3Options, die3Weights);
 
-// Display the result of the roll in the text view
-                resultTextView.setText("Die 3: " + wrand.next());
-            }
-        });
-
-        die4Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Roll the die using a weighted random generator
-                rollDie(die4Options, die4Weights);
-
-                // Display the result of the roll in the text view
-                resultTextView.setText("Die 4: " + wrand.next());
-            }
+            // Display the result of the roll in the text view
+            resultTextView.setText("Die 3: " + wrand.next());
         });
 
         // Set the on click listener for the reset button
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Reset the text view
-                resultTextView.setText("");
-            }
+        resetButton.setOnClickListener(v -> {
+            // Roll the die for each button with the appropriate options and weights
+            rollDie(die1Options, die1Weights);
+            String die1Result = wrand.next();
+            rollDie(die2Options, die2Weights);
+            String die2Result = wrand.next();
+            rollDie(die3Options, die3Weights);
+            String die3Result = wrand.next();
+
+            // Display the result of the roll in the text view
+            resultTextView.setText("Die 1: " + die1Result + "\nDie 2: " + die2Result + "\nDie 3: " + die3Result);
         });
     }
 
@@ -110,8 +101,8 @@ public class RollerbladingDiceActivity extends AppCompatActivity {
 
     // Custom class for a weighted random generator
     private class WeightedRandomGenerator {
-        private ArrayList<String> options = new ArrayList<String>();
-        private ArrayList<Integer> weights = new ArrayList<Integer>();
+        private final ArrayList<String> options = new ArrayList<>();
+        private final ArrayList<Integer> weights = new ArrayList<>();
         private int totalWeight = 0;
 
         // Method to add an option and its weight to the generator
